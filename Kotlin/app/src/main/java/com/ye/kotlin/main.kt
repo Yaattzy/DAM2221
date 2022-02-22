b62da2e033865b098eac68938c7d112dc849e497
yaattzy <yeratzyen@gmail.com>
2022-02-22 14:53:18 -0700

    Funciones, if/else, when

--- Kotlin/app/src/main/java/com/ye/kotlin/main.kt
+++ Kotlin/app/src/main/java/com/ye/kotlin/main.kt
@@ -54,6 +54,66 @@ fun main() {
     // enum class
     println(Dias.Lunes.numeroDia)
 
+    println("**********************")
+    println("FUNCIONES")
+
+    println(suma(2,3))
+    println(suma2(2,3))
+    imprimirSuma(4,7)
+
+    println("**********************")
+    println("CONTROL DE FLUJO")
+
+    println("----IF/ELSE")
+
+    var num1 = 1
+    var num2 = 5
+    var numMax: Int
+
+    numMax = if(num1 > num2) {
+        num1
+    } else {
+        num2
+    }
+
+    println("El mayor es $numMax")
+
+    num1 = 6
+    if(num1 > num2) numMax = num1 else numMax = num2
+
+    println("El mayor es $numMax")
+
+    num1 = 3
+    numMax = if (num1 > num2) num1 else num2
+
+    println("El mayor es $numMax")
+
+    val numEstudiantes = 20
+
+    if(numEstudiantes in 1..100){
+        println(numEstudiantes)
+    }
+
+    println("---WHEN")
+
+    var r = 20
+
+    when(r){
+        1 -> {println("r es 1")}
+        2 -> {println("r es 2")}
+        3 -> {println("r es 3")}
+        5, 6 -> {println("r es 5 o 6")}
+        else -> {println("r no es ninguna de las opciones")}
+    }
+
+    when(r) {
+        in 1..10 -> println("r esta en el rango")
+        !in 10..20 -> println("r no esta en este rango")
+        else -> println("No se encontro una opcion")
+    }
+
+
+
 }
 
 // CLASE PERSONA
@@ -82,4 +142,16 @@ enum class Dias(val numeroDia: Int, val otraCosa: String) {
     Lunes(1, "hola"),
     Martes(2, "adios"),
     Miercoles(3, "nose")
+}
+
+// Funciones
+
+fun suma(numero1: Int, numero2: Int): Int {
+    return numero1 + numero2
+}
+
+fun suma2(numero1: Int, numero2: Int) = numero1 + numero2
+
+fun imprimirSuma(a: Int, b: Int) : Unit {
+    println("La suma de a: $a y b: $b es ${a+b}")
 }
