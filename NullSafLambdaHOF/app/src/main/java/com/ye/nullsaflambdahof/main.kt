ece59f7f0c540781c4f582be4eda0adba888a2e7
yaattzy <yeratzyen@gmail.com>
2022-02-28 14:51:43 -0700

    Null safety y lambdas!

---
+++ NullSafLambdaHOF/app/src/main/java/com/ye/nullsaflambdahof/main.kt
@@ -0,0 +1,27 @@
+package com.ye.nullsaflambdahof
+
+fun main() {
+
+    /****NULL SAFETY***/
+    var mensaje: String? = "hola"
+
+    println(mensaje?.length ?: "adios".length)
+
+    mensaje?.let {
+        println(it)
+    }
+
+    /******EXPRESIONES LAMBDA*****/
+
+    val texto: (String) -> String = {palabra -> palabra}
+
+    println(texto("notengoidea"))
+
+    val cuadrado: (Int) -> Int = {numero -> numero * numero}
+    println(cuadrado(2))
+
+    val suma: (Int, Int) -> String = { numero1, numero2 -> "La suma es ${numero1 + numero2}"}
+    println(suma(2, 4))
+
+}
+
