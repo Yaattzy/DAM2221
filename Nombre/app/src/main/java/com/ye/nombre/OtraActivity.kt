1af744cd7b6d95689e4508de9ccfcf22e5c82b2b
yaattzy <yeratzyen@gmail.com>
2022-03-03 14:47:44 -0700

    nada

---
+++ Nombre/app/src/main/java/com/ye/nombre/OtraActivity.kt
@@ -0,0 +1,11 @@
+package com.ye.nombre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otra)
+    }
+}
