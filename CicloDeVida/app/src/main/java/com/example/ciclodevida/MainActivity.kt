a795e46a05c9d01e29b709c2e408ecca4780acf6
Docente Moviles <docentemoviles@MAESTRO.local>
2022-04-06 14:48:53 -0600

    Ciclo de vida con cancion

--- CicloDeVida/app/src/main/java/com/example/ciclodevida/MainActivity.kt
+++ CicloDeVida/app/src/main/java/com/example/ciclodevida/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.ciclodevida
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.ciclodevida.databinding.ActivityMainBinding
 
@@ -9,9 +11,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     var mensaje: String = "HOLA"
+    private var mediaPlayer: MediaPlayer? = null
+    var position: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("Ciclo de vida", "onCreate")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -22,6 +28,48 @@ class MainActivity : AppCompatActivity() {
             mensaje="OTRA COSA"
             Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+         Log.i("Ciclo de vida", "onStart")
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.musica)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Ciclo de vida", "onResume")
+        mediaPlayer?.seekTo(position)
+        mediaPlayer?.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Ciclo de vida", "onPause")
+        mediaPlayer?.pause()
+
+        if(mediaPlayer != null) {
+            position = mediaPlayer!!.currentPosition
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Ciclo de vida", "onStop")
+        // Liberación de recursos
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Ciclo de vida", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Ciclo de vida", "onDestroy")
     }
 }
