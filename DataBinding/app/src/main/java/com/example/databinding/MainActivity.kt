2787ed62ec65a1ade4abc844d8e7a32aeaa56ce2
Docente Moviles <docentemoviles@MAESTRO.local>
2022-04-26 14:54:29 -0600

    DataBinding

---
+++ DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.databinding
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.saludo = "bIENVENIdo"
+    }
+}
