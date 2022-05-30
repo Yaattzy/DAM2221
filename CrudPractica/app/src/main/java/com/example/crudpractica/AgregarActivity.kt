be353297bcf3062a6132f5cc3b6a93998e9353df
Docente Moviles <docentemoviles@MAESTRO.local>
2022-05-30 13:52:32 -0600

    MVVM

---
+++ CrudPractica/app/src/main/java/com/example/crudpractica/AgregarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.crudpractica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar)
+    }
+}
--- CrudPractica/app/src/main/java/com/example/crudpractica/MusicViewModel.kt
+++ CrudPractica/app/src/main/java/com/example/crudpractica/MusicViewModel.kt
@@ -1,14 +1,43 @@
 package com.example.crudpractica
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class MusicViewModel : ViewModel() {
 
-        private val musicRepository: MusicRepository = MusicRepository()
+    private val musicRepository: MusicRepository = MusicRepository()
+
+    var _id = MutableLiveData<Long>()
+    var name = MutableLiveData<String>()
+    var author = MutableLiveData<String>()
+    var genre = MutableLiveData<String>()
+    var year = MutableLiveData<String>()
 
     val musicList = musicRepository.allMusic // Obtener todos los objetos
+
+
+    fun save() {
+        insert(
+            Music(
+                id= null,
+                name.value.toString(),
+                author.value.toString(),
+                genre.value.toString(),
+                year.value.toString()
+            )
+        )
+        clean()
+    }
+
+    fun clean() {
+        _id.value = 0
+        name.value = ""
+        author.value = ""
+        year.value = ""
+        genre.value = ""
+    }
 
     // Métodos del repositorio
 
@@ -30,7 +59,7 @@ class MusicViewModel : ViewModel() {
         }
     }
 
-    fun deleteAll(music: Music){
+    fun deleteAll(){
         viewModelScope.launch {
             musicRepository.deleteAllMusic()
         }
